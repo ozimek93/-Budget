11baf96995cd26eaed3d961eaddaa5438c4177fd
ozimek93 <75270472+ozimek93@users.noreply.github.com>
2021-01-13 18:31:40 +0100

    Add files via upload

---
+++ Budżet/Budżet/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budżet
+{
+    public class Transaction
+    {
+        public string Name { get; set; }
+
+        public decimal Value { get; set; }
+
+        public int TypeId { get; set; }
+    }
+}
